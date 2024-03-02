a42475b6e03b6704490109f2c4edc15d0a2e12da
Tony <tony.guan@etu.u-paris.fr>
2024-03-02 22:14:06 +0100

    update

--- controllers.js
+++ controllers.js
@@ -1,30 +1,29 @@
 const Book = require('../model/livre');
 
 exports.createBookSchema = {
-        body: {
-            type: 'object',
-            required: ['title', 'author'],
-            properties: {
-                title: { type: 'string' },
-                author: { type: 'string' },
-                description: { type: 'string', default: '' },
-                format: {
-                    type: 'string',
-                    enum: ['poche', 'manga', 'audio'],
-                    default: 'poche'
-                }
-            },
-            additionalProperties: false
-        }
+    body: {
+        type: 'object',
+        required: ['title', 'author'],
+        properties: {
+            title: { type: 'string' },
+            author: { type: 'string' },
+            description: { type: 'string', default: '' },
+            format: {
+                type: 'string',
+                enum: ['poche', 'manga', 'audio'],
+                default: 'poche'
+            }
+        },
+        additionalProperties: false
+    }
 };
 
 exports.createBook = async (request, reply) => {
-        }
     try {
         const book = new Book(request.body);
         const savedBook = await book.save();
         reply.code(201).send(savedBook);
     } catch (err) {
-        reply.code(500).send(err);
+        reply.code(500).send(err); 
     }
 };
